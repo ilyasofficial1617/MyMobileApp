0dce62e0c2fd707c50c4efb60e2cbf2a8aa4a239
ilyasofficial1617 <ilyasofficial1617@gmail.com>
2020-09-23 15:14:55 +0700

    added task adder activity

---
+++ app/src/main/java/com/yudhistira/mymobileapp/TaskAdder.java
@@ -0,0 +1,13 @@
+package com.yudhistira.mymobileapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TaskAdder extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.task_adder);
+    }
+}
